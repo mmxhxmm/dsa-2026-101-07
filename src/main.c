51630c1ada5a5faa26d120d32d36a0e9726ef919
Pinda <pindali719@gmail.com>
2026-04-11 19:26:01 +0200

    Add new file and update main

--- src/main.c
+++ src/main.c
@@ -4,13 +4,84 @@
 #include <stdlib.h>
 #include <sys/stat.h>
 
+typedef struct{
+
+  char street_name[100];
+  int street_num;
+  double lat;
+  double lon;
+  struct Houses* next_house;
+
+}Houses;
+
 void createaleak() {
   char *foo = malloc(20 * sizeof(char));
   printf("Allocated leaking string: %s", foo);
 }
 
+void menu(){
+
+  printf("Enter map name (xs_1, xs_2, md_1, lg_1, xl_1, 2xl_1)");
+
+  char map_name[20];
+
+  scanf("%s", map_name);
+
+  int where;
+  
+  printf("\nWhere are you? Address (1), Place (2) or Coordinate (3)?");
+
+  
+  scanf("%d", &where);
+  
+  while(where<1 || where>3){
+    printf("\nInvalid number. Enter again");
+    scanf("%d", &where);
+  }
+
+  switch(where){
+    case 1:
+
+      int street_num;
+      char street_name[50];
+      double lat, lon;
+
+
+      printf("\nEnter street name (e.g. \"Carrer de Roc Boronat\"):");
+      getchar();
+      fgets(street_name, 50, stdin);
+      street_name[strcspn(street_name, "\n")]= '\0';
+
+      printf("\nEnter street number (e.g. "138"):");
+      scanf("%d", &street_num);
+
+
+      printf("Found at (%f, %f)", lat, lon);
+
+      break;
+
+    case 2:
+
+      printf("\nNot implemented yet.");
+      break;
+
+    case 3:
+      printf("\nNot implemented yet.");
+      break; 
+  }
+
+
+  return;
+}
+
+
+
+
+
 int main() {
   printf("*****************\nWelcome to DSA!\n*****************\n");
+
+  menu();
 
   // how to import and call a function
   printf("Factorial of 4 is %d\n", fact(4));
